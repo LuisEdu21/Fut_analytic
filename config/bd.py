138dc8d073a14fe6c739cd3d3792f3d93eed2bbf
Luis Eduardo Gomes <luiseduardotgs2001@hotmail.com>
2023-06-03 20:27:38 -0300

    Criação da tabela

--- config/bd.py
+++ config/bd.py
@@ -11,6 +11,37 @@ def conexao_Com_Banco():
                 password=os.getenv('pass_DB'))
     return conexao
 
+def criar_Tabela():
+    conexao = conexao_Com_Banco()
+
+    SQL_Criacao_Tabela = """CREATE TABLE public.tabela_fut (
+	liga varchar NULL,
+	colocacao int4 NULL,
+	clube varchar NULL,
+	pts int4 NULL,
+	pj int4 NULL,
+	vit int4 NULL,
+	e int4 NULL,
+	der int4 NULL,
+	gm int4 NULL,
+	gc int4 NULL,
+	sg int4 NULL,
+	datahora timestamp NULL)"""
+    
+    cursor = conexao.cursor()
+
+    with open("config/Tabela.txt", "r") as arquivo:
+        resultado = arquivo.read()
+
+    if resultado == 'N':
+        cursor.execute(SQL_Criacao_Tabela)
+        #conexao.commit()
+        with open("config/Tabela.txt", "w") as arquivo:
+            arquivo.write("S")
+    else:
+        print('Tabela já existe')
+
+    return print('Finalizado processo...')
 
 def Inserirtime(liga,colocacao,clube,pts,pj,vit,empate,der,gm,gc,sg):
     con = conexao_Com_Banco()
@@ -20,6 +51,3 @@ def Inserirtime(liga,colocacao,clube,pts,pj,vit,empate,der,gm,gc,sg):
     cur.execute(sql)
     con.commit()
     print ("Conclusion")
-
-
-
