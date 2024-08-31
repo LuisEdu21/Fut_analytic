460a4032e338ebdb3ea7f3846bfc4ef2d1556966
Luis Eduardo Gomes <luiseduardotgs2001@hotmail.com>
2024-08-30 23:26:51 -0300

    regularizar

--- config/funcoes.py
+++ config/funcoes.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 from time import sleep
 import bd as ac
@@ -17,9 +18,25 @@ def buscar_HTML(Link):
 
     return html
 
+#Função com requests. Existe um problema os dados nao carrega e no postman ta erro 404
+def buscar_HTML_BS4(Link):
+    #Navegador selenium
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+    }
+
+    site = requests.get(Link, headers=headers)
+
+    sleep(10)
+
+    #Transformando a Pagina em HTML document
+    html = BeautifulSoup(site.content, 'html.parser')
+
+    return html
+
 def brasileirao_Serie_A():
     Link = 'https://www.google.com/search?gs_ssp=eJzj4tTP1TdIy8s2LzRg9OJOKkoszsxJzSxKzAcAXOUICQ&q=brasileirao&oq=Bras&aqs=chrome.1.69i57j46i39i650j69i60l2j69i65l3j69i60.2527j1j7&sourceid=chrome&ie=UTF-8#sie=lg;/g/11jspy1hvm;2;/m/0fnk7q;st;fp;1;;;'
-
+    
     html = buscar_HTML(Link)
 
     print('Pegando o time!')
@@ -43,7 +60,6 @@ def brasileirao_Serie_A():
             Parte_1 = html.find_all('td',class_="e9fBA xkW0Cc snctkc xL0E7c")
             Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc xL0E7c")
             for i in range (1):
-                sleep(2)
                 Pontuação = Parte_1[y].get_text()
                 Jogos = Parte_1[y+1].get_text()
                 Vitoria = Parte_1[y+2].get_text()
@@ -52,7 +68,6 @@ def brasileirao_Serie_A():
                 Derrota = Parte_2[y+1].get_text()
                 Gols_Marcado = Parte_2[y+2].get_text()
                 Gols_Contra = Parte_2[y+3].get_text()
-                sleep(2)
                 z = z + 1
                 c = 1
                 ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)
@@ -61,7 +76,6 @@ def brasileirao_Serie_A():
             Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc bWoKCf")
             for i in range (1):
                 y = 0
-                sleep(2)
                 Pontuação = Parte_1[y].get_text()
                 Jogos = Parte_1[y+1].get_text()
                 Vitoria = Parte_1[y+2].get_text()
@@ -70,25 +84,14 @@ def brasileirao_Serie_A():
                 Derrota = Parte_2[y+1].get_text()
                 Gols_Marcado = Parte_2[y+2].get_text()
                 Gols_Contra = Parte_2[y+3].get_text()
-                print(Pontuação)
-                print(Jogos)
-                print(Vitoria)
-                print(Empates)
-                print(Derrota)
-                print(Gols_Marcado)
-                print(Gols_Contra)
-                print(Saldo_de_Gol)
-                sleep(2)
                 z = z + 1
                 c = 20
-                print(c)
                 ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)
                 break
         elif z < 19:
             Parte_1 = html.find_all('td',class_="e9fBA xkW0Cc snctkc")
             Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc")
             for i in range (1):
-                sleep(2)
                 Pontuação = Parte_1[y].get_text()
                 Jogos = Parte_1[y+1].get_text()
                 Vitoria = Parte_1[y+2].get_text()
@@ -97,19 +100,9 @@ def brasileirao_Serie_A():
                 Derrota = Parte_2[y+1].get_text()
                 Gols_Marcado = Parte_2[y+2].get_text()
                 Gols_Contra = Parte_2[y+3].get_text()
-                print(Pontuação)
-                print(Jogos)
-                print(Vitoria)
-                print(Empates)
-                print(Derrota)
-                print(Gols_Marcado)
-                print(Gols_Contra)
-                print(Saldo_de_Gol)
-                sleep(2)
                 y = y + 4
                 z = z + 1
                 c = c + 1 
-                print(c)
                 ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)       
         else:
             print("Tabela Concluida")
@@ -589,3 +582,132 @@ def Bundesliga():
         x = x + 1
 
     return print('Bundesliga concluido...')
+
+#Função para testa com o request
+def brasileirao_Serie_A_BS4():
+
+    Link = 'https://www.sofascore.com/tournament/football/brazil/brasileiro-serie-a/325'
+    
+    html = buscar_HTML_BS4(Link)
+
+    #print(html)
+    print('Pegando o time!')
+
+    Time = html.find_all('span',class_="sc-bqWxrE uTWnT")
+
+    print(Time)
+
+    liga = html.find('div',class_="ofy7ae").get_text()
+    
+    print(liga)
+
+    c = 0
+    x = 0
+    y = 0
+    z = 0
+
+    for Clubes in Time:
+        Clube = Time[x].get_text()
+        print(Clube)
+
+        if z == 0:
+            Parte_1 = html.find_all('td',class_="e9fBA xkW0Cc snctkc xL0E7c")
+            Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc xL0E7c")
+            for i in range (1):
+                Pontuação = Parte_1[y].get_text()
+                Jogos = Parte_1[y+1].get_text()
+                Vitoria = Parte_1[y+2].get_text()
+                Saldo_de_Gol = Parte_1[y+3].get_text()
+                Empates = Parte_2[y].get_text()
+                Derrota = Parte_2[y+1].get_text()
+                Gols_Marcado = Parte_2[y+2].get_text()
+                Gols_Contra = Parte_2[y+3].get_text()
+                z = z + 1
+                c = 1
+                ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)
+        elif z == 19:
+            Parte_1 = html.find_all('td',class_="e9fBA xkW0Cc snctkc bWoKCf")
+            Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc bWoKCf")
+            for i in range (1):
+                y = 0
+                Pontuação = Parte_1[y].get_text()
+                Jogos = Parte_1[y+1].get_text()
+                Vitoria = Parte_1[y+2].get_text()
+                Saldo_de_Gol = Parte_1[y+3].get_text()
+                Empates = Parte_2[y].get_text()
+                Derrota = Parte_2[y+1].get_text()
+                Gols_Marcado = Parte_2[y+2].get_text()
+                Gols_Contra = Parte_2[y+3].get_text()
+                print(Pontuação)
+                print(Jogos)
+                print(Vitoria)
+                print(Empates)
+                print(Derrota)
+                print(Gols_Marcado)
+                print(Gols_Contra)
+                print(Saldo_de_Gol)
+                z = z + 1
+                c = 20
+                print(c)
+                ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)
+                break
+        elif z < 19:
+            Parte_1 = html.find_all('td',class_="e9fBA xkW0Cc snctkc")
+            Parte_2 = html.find_all('td', class_="e9fBA xkW0Cc snctkc kRLtzc")
+            for i in range (1):
+                Pontuação = Parte_1[y].get_text()
+                Jogos = Parte_1[y+1].get_text()
+                Vitoria = Parte_1[y+2].get_text()
+                Saldo_de_Gol = Parte_1[y+3].get_text()
+                Empates = Parte_2[y].get_text()
+                Derrota = Parte_2[y+1].get_text()
+                Gols_Marcado = Parte_2[y+2].get_text()
+                Gols_Contra = Parte_2[y+3].get_text()
+                print(Pontuação)
+                print(Jogos)
+                print(Vitoria)
+                print(Empates)
+                print(Derrota)
+                print(Gols_Marcado)
+                print(Gols_Contra)
+                print(Saldo_de_Gol)
+                y = y + 4
+                z = z + 1
+                c = c + 1 
+                print(c)
+                ac.Inserirtime(liga,c,Clube,Pontuação,Jogos,Vitoria,Empates,Derrota,Gols_Marcado,Gols_Contra,Saldo_de_Gol)       
+        else:
+            print("Tabela Concluida")
+
+        x = x + 1
+
+    return print('Brasileirao concluido...')
+
+def brasileirao_Serie_A_JOGOS():
+    Link = 'https://www.google.com/search?gs_ssp=eJzj4tTP1TdIy8s2LzRg9BJOKkoszsxJzSzKVyg-vLIoM1UhEQCuIwtw&q=brasileiro+s%C3%A9rie+a&rlz=1C1PNBB_enBR1041BR1041&oq=brasileiro+s%C3%A9rie+&aqs=chrome.2.69i57j46i39i650j46i131i433i512j46i131i340i433i512l2j0i131i433i512l2j69i60.3901j0j7&sourceid=chrome&ie=UTF-8#sie=lg;/g/11jspy1hvm;2;/m/0fnk7q;mt;fp;1;;;'
+    
+    html = buscar_HTML(Link)
+
+    print('Pegando os jogos do brasileirao!')
+
+    Rodadas = html.find_all('div', class_= "OcbAbf")
+
+    x = 0
+
+    for i in Rodadas:
+        Rodada = html.find('div', class_= "OcbAbf").get_text
+        print(Rodada)
+        x = x+1 
+        
+
+    Times = html.find_all('div',class_="liveresults-sports-immersive__hide-element")
+
+    Resultado = html.find_all('div',class_="imspo_mt__tt-w")
+
+    print(Times)
+    print(Resultado)
+
+
+    return print('Brasileirao concluido...')
+
+brasileirao_Serie_A_JOGOS()
